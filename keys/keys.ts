d56f11f19a6197954ced0b363fb9bed27a6e17bd
okimjae <ijae@live.com>
2021-08-31 01:36:23 -0300

    first commit

---
+++ keys/keys.ts
@@ -0,0 +1,3 @@
+const THE_NEWS_API_KEY = "7cea53fd9ba847f8bd83fa4514ede956";
+
+export default THE_NEWS_API_KEY;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider, theme, CSSReset } from "@chakra-ui/react";
+
+const customTheme = {
+  ...theme,
+  colors: {
+    ...theme.colors,
+    spacejelly: "#692ba8",
+  },
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
-export default MyApp
+export default MyApp;
